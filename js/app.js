c28850ead889815fd5cd38fbe941110d18cab4f1
allansgsdev <allanscgomes.dev@gmail.com>
2025-02-27 20:57:37 -0300

    Upload dos arquivos iniciais

---
+++ js/app.js
@@ -0,0 +1,22 @@
+// *PÁGINA DE DESAFIOS*
+
+// Desafio: Compra de Ingressos
+// Autor: Allan Gomes
+// Curso: Lógica de programação - Praticando com desafios
+// Instituição: Alura
+
+
+// DECLARAÇÃO E ATRIBUÇÃO DE VARIÁVEIS
+
+
+// DECLARAÇÃO DE FUNÇÕES
+
+
+// PROGRAMA PRINCIPAL
+const input = document.querySelector('input');
+
+input.addEventListener("keyup", ({ key }) => {
+    if (key === "Enter") {
+
+    }
+})
