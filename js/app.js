6e878e9f8299c8651969872077a84aa68494961e
allansgsdev <allanscgomes.dev@gmail.com>
2025-02-27 21:18:37 -0300

    Inserção da versão 1.0 do Programa

--- js/app.js
+++ js/app.js
@@ -7,16 +7,60 @@
 
 
 // DECLARAÇÃO E ATRIBUÇÃO DE VARIÁVEIS
-
+let campoIngresso = document.getElementById('tipo-ingresso');
+let campoQuantidade = document.getElementById('qtd');
+let campoQuantidadePista = document.getElementById('qtd-pista');
+let campoQuantidadeSuperior = document.getElementById('qtd-superior');
+let campoQuantidadeInferior = document.getElementById('qtd-inferior');
 
 // DECLARAÇÃO DE FUNÇÕES
+function comprar() {
+    var local = campoIngresso.value;
+    var quantidade = campoQuantidade.value;
 
+    if (!local || local.trim() === '') {
+        alert('Insira um tipo de ingresso válido.');
+    } else {
+        if (!isNaN(quantidade) && quantidade >= 1) {
+            if (confirm(`Você realmente deseja comprar este ingresso?\n\n${quantidade}x ${local}`)) {
+                if (local == 'pista') {
+                    if (campoQuantidadePista.textContent >= quantidade) {
+                        campoQuantidadePista.textContent -= quantidade;
+                    } else {
+                        alert('Ingresso indisponível!');
+                    }
+                }
+                if (local == 'superior') {
+                    if (campoQuantidadeSuperior.textContent >= quantidade) {
+                        campoQuantidadeSuperior.textContent -= quantidade;
+                    } else {
+                        alert('Ingresso indisponível!');
+                    }
+                }
+                if (local == 'inferior') {
+                    if (campoQuantidadeInferior.textContent >= quantidade) {
+                        campoQuantidadeInferior.textContent -= quantidade;
+                    } else {
+                        alert('Ingresso indisponível!');
+                    }
+                }
+            }
+        } else {
+            alert('Insira uma quantidade válida');
+        }
+    }
+
+    return
+}
 
 // PROGRAMA PRINCIPAL
+campoQuantidadePista.textContent = 100;
+campoQuantidadeSuperior.textContent = 200;
+campoQuantidadeInferior.textContent = 400;
 const input = document.querySelector('input');
 
 input.addEventListener("keyup", ({ key }) => {
     if (key === "Enter") {
-
+        comprar();
     }
 })
