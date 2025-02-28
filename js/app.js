b703e94457424aa1db60f9273e9ae3c87ef43b2c
allansgsdev <allanscgomes.dev@gmail.com>
2025-02-27 21:36:39 -0300

    Corringindo bug na comparação de valores e ajustando mensagem de ingresso indisponível

--- js/app.js
+++ js/app.js
@@ -14,9 +14,17 @@ let campoQuantidadeSuperior = document.getElementById('qtd-superior');
 let campoQuantidadeInferior = document.getElementById('qtd-inferior');
 
 // DECLARAÇÃO DE FUNÇÕES
+function singularOuPlural(comparador, singular, plural) {
+    var comparar = comparador > 1 ? singular : plural;
+    return comparar
+}
+
 function comprar() {
     var local = campoIngresso.value;
-    var quantidade = campoQuantidade.value;
+    var quantidade = parseInt(campoQuantidade.value);
+    var quantidadePista = parseInt(campoQuantidadePista.textContent);
+    var quantidadeInferior = parseInt(campoQuantidadeInferior.textContent);
+    var quantidadeSuperior = parseInt(campoQuantidadeSuperior.textContent);
 
     if (!local || local.trim() === '') {
         alert('Insira um tipo de ingresso válido.');
@@ -24,33 +32,32 @@ function comprar() {
         if (!isNaN(quantidade) && quantidade >= 1) {
             if (confirm(`Você realmente deseja comprar este ingresso?\n\n${quantidade}x ${local}`)) {
                 if (local == 'pista') {
-                    if (campoQuantidadePista.textContent >= quantidade) {
+                    if (quantidadePista >= quantidade) {
                         campoQuantidadePista.textContent -= quantidade;
                     } else {
-                        alert('Ingresso indisponível!');
+                        alert(`Quantidade indisponível para ${local}.\n\n${quantidadePista} ${singularOuPlural(quantidade, 'ingressos disponíveis', 'ingresso disponível')}`);
                     }
                 }
                 if (local == 'superior') {
-                    if (campoQuantidadeSuperior.textContent >= quantidade) {
+                    if (quantidadeSuperior >= quantidade) {
                         campoQuantidadeSuperior.textContent -= quantidade;
                     } else {
-                        alert('Ingresso indisponível!');
+                        alert(`Quantidade indisponível para ${local}.\n\n${quantidadeSuperior} ${singularOuPlural(quantidade, 'ingressos disponíveis', 'ingresso disponível')}`);
                     }
                 }
                 if (local == 'inferior') {
-                    if (campoQuantidadeInferior.textContent >= quantidade) {
+                    if (quantidadeInferior >= quantidade) {
                         campoQuantidadeInferior.textContent -= quantidade;
                     } else {
-                        alert('Ingresso indisponível!');
+                        alert(`Quantidade indisponível para ${local}.\n\n${quantidadeInferior} ${singularOuPlural(quantidade, 'ingressos disponíveis', 'ingresso disponível')}`);
                     }
                 }
-                campoQuantidade.value = '';
             }
         } else {
             alert('Insira uma quantidade válida');
         }
     }
-
+    campoQuantidade.value = '';
     return
 }
 
